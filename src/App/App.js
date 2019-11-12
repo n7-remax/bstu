165678f90258ba2d84952d61b441863b55039ea7
N7_Remax <ditritusa@gmail.com>
2019-11-12 19:33:36 +0200

    added header module

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Intro from './Intro/Intro';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Intro></Intro>
     </div>
   );
--- src/App/Intro/Intro.js
+++ src/App/Intro/Intro.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './intro.css'
 
 const Intro = () =>
-    <section className="Intro">
+    <section className="intro">
     </section>
 
 export default Intro
