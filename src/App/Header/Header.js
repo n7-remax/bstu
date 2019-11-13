8eee3b1d996f7a9c7fe4f61ed53ebbac9ddd1d7d
N7_Remax <ditritusa@gmail.com>
2019-11-13 16:54:30 +0200

    adeed header content, logo, background image

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -3,7 +3,18 @@ import React from 'react'
 import './header.css'
 
 const Header = () =>
-    <header className="header">
+    <header className="header test">
+        <div className="logo-place">
+            <a href="#"><img className="logo" src={require('./logo.png')}></img></a>
+            <h1>Bryansk State Technical University</h1>
+        </div>
+        <nav className="nav">
+            <ul>
+                <li>Study with us</li>
+                <li>About us</li>
+                <li>Integration program</li>
+            </ul>
+        </nav>
     </header>
 
 export default Header
