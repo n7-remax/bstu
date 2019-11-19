e9d5e87663f239b87c02b96172d94b8e50ec6126
N7_Remax <ditritusa@gmail.com>
2019-11-19 18:24:27 +0200

    fix img and button

--- src/App/Intro/Intro.js
+++ src/App/Intro/Intro.js
@@ -4,6 +4,16 @@ import './intro.css'
 
 const Intro = () =>
     <section className="intro">
+        <div className="headline"><p>The place to</p></div>
+        <div class="make" aria-label="Make"><p>
+          <span>M</span><span>A</span><span>K</span><span>E</span>
+        </p></div>
+        <div class="words-container">
+          <p class="words-backgrounded">Your future</p>
+        </div>
+        <div className="button-place">
+        <a class="button hollow" id="test" href="#course-finder">Explore our courses</a>
+        </div>
     </section>
 
 export default Intro
