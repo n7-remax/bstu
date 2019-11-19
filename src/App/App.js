8b9ee374867316ebecaa3e1d2df84871beb38d1d
N7_Remax <ditritusa@gmail.com>
2019-11-19 18:35:43 +0200

    Created city module

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Intro from './Intro/Intro';
 import Header from './Header/Header';
+import City from './City/City';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Intro></Intro>
+      <City></City>
     </div>
   );
 }
---
+++ src/App/City/City.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './city.css'
+
+const City = () =>
+    <section className="city">
+        <div class="video-bar flint">
+            <div class="row column">
+                <blockquote>“It’s great studying in Bryansk - I fell in love with the city at first sight.”</blockquote>
+            </div>
+            <div>
+                <video url="https://youtu.be/pxPqZmaQ6wc"></video>
+            </div>
+        </div>
+    </section>
+
+export default City
