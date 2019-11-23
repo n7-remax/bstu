456bf8e043f5e1712595cc8e6ccd8a2ac741a5bf
N7_Remax <ditritusa@gmail.com>
2019-11-23 20:30:41 +0200

    add content to city module

--- src/App/City/City.js
+++ src/App/City/City.js
@@ -4,12 +4,15 @@ import './city.css'
 
 const City = () =>
     <section className="city">
-        <div class="video-bar flint">
-            <div class="city-container">
-                <blockquote>“It’s great studying in Bryansk - I fell in love with the city at first sight.”</blockquote>
-            </div>
-            <div>
-            </div>
+        <div class="city-container">
+            <blockquote>“Bryansk is a City of Military Glory - Here i can literally touch the history.”</blockquote>
+        </div>
+        <div className="video-frame">
+            <iframe src="https://www.youtube.com/embed/qlwpBcZEt5s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+            </iframe>
+        </div>
+        <div className="buton-place">
+            <a className="buton" href="#">Learn more about Bryansk</a>
         </div>
     </section>
 
