c1d250997303c33b997904bdad3cc97a12a49457
N7_Remax <ditritusa@gmail.com>
2019-11-24 23:15:31 +0200

    added content to bryansk

--- src/App/Main/Bryansk/Bryansk.js
+++ src/App/Main/Bryansk/Bryansk.js
@@ -4,7 +4,17 @@ import './bryansk.css'
 
 const Bryansk = () =>
     <section className="bryansk">
-        <p>Hello</p>
+        <div className="arms-place">
+            <img src={require('./bryansk-city-arms.png')} alt="city-arms"></img>
+        </div>
+        <p>Bryansk is located on the western edge of the Central Russian Upland, on both banks of the Desna River. This is one of the few Russian cities which age exceeds 1,000 years. Bryansk consists of four large districts located at some distance from each other and is surrounded by forests, which positively affects the air quality in the city. The City Day of Bryansk is celebrated on September 17.</p>
+        <img className="bryansk-images" src={require('../../../common/images/bryansk.jpg')} alt="logo"></img>
+        <p>Bryansk is located on the western edge of the Central Russian Upland, on both banks of the Desna River. This is one of the few Russian cities which age exceeds 1,000 years. Bryansk consists of four large districts located at some distance from each other and is surrounded by forests, which positively affects the air quality in the city. The City Day of Bryansk is celebrated on September 17.
+The climate is temperate continental. Winters are characterized by unstable weather: from severe frosts to prolonged thaws, summers are wet and warm, but extreme heat is rare. The average temperature in January is minus 7.7 degrees Celsius, in July - plus 18.6 degrees Celsius. The best time to travel to Bryansk is from May to September.
+Bryansk is a large industrial city. Metalworking is the main industry of the local industry. Chemical, electrical, electronic, woodworking, textile, and food industries are also relatively well developed. More than 1,200 enterprises of Bryansk produce diesel locomotives, freight cars, graders, pavers and other road equipment, agricultural machinery, construction materials, clothing, and other industrial products.
+The city is a major railway junction through which railway lines run to Moscow, Kiev, Kharkov, Gomel, Smolensk, Orel, and Vyazma. The Bryansk Airport provides regular flights to Moscow, St. Petersburg, Sochi, Krasnodar. The public transport system of Bryansk includes buses, trolleybuses, trams, and minibuses.
+A lot of people come to Bryansk purposefully to improve their health in the local sanatoriums. On the outskirts of Bryansk in the thick of forests there are health camps for children, sanatoriums and recreation centers for everyone.
+This city has a number of noteworthy tourist sites: Orthodox churches, memorials in honor of the heroes of the Second World War, museums, and theaters. The Dvina flowing through Bryansk is a picturesque river, which right bank rises above the water surface and the left one is dotted with small sand spits. Most of the sights of Bryansk are located in its historical center on the right bank of the Desna.</p>
     </section>
 
 export default Bryansk
