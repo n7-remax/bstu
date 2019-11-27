cd0dbdbdc3f568364f16fc99163108bc8301b80f
N7_Remax <ditritusa@gmail.com>
2019-11-27 02:08:43 +0200

    created University module

---
+++ src/App/Main/University/University.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './university.css'
+
+const University = () =>
+    <div className="university">
+    </div>
+
+export default University
