ee0ac5f120b852dcb3d62426a5be50c583d796a4
N7_Remax <ditritusa@gmail.com>
2019-11-27 14:38:50 +0200

    added copyright to footer

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -4,38 +4,46 @@ import './footer.css'
 
 const Footer = () =>
     <footer className="footer" id="footer">
-        <div className="row footer-info">
-            <div className="col-five">
-                <p>
-                    <span>Address:</span>
-                    <span>241035, Bryansk, ul Institutskaya, 16,</span>
-                    <span>Administrative building, 2nd floor</span>
-                    <span>RM. А210, А230, А231</span>
-                </p>
-                <p>
-                    <span>Email:</span>
-                    <a href="mailto:pk@tu-bryansk.ru">pk@tu-bryansk.ru</a>
-                </p>
-                <p>
-                    <span>Phones:</span>
-                    <span>Admissions: +7 (4832) 58-82-64</span>
-                    <span>Responsible Secretary of the admission</span>
-                    <span>Vysotsky Andrey: +7 (4832) 51-19-97</span>
-                    <span>First Vice-rector for academic Affairs</span>
-                    <span>Prokofiev Alexander: +7 (4832) 56-25-09</span>
-                    <span>Preparatory courses</span>
-                    <span>Lebedeva Tatyana: +7 (4832) 58-82-42</span>
-                </p>
+        <div className="row">
+            <div className="footer-info">
+                <div className="col-five">
+                    <p>
+                        <span>Address:</span>
+                        <span>241035, Bryansk, ul Institutskaya, 16,</span>
+                        <span>Administrative building, 2nd floor</span>
+                        <span>RM. А210, А230, А231</span>
+                    </p>
+                    <p>
+                        <span>Email:</span>
+                        <a href="mailto:pk@tu-bryansk.ru">pk@tu-bryansk.ru</a>
+                    </p>
+                    <p>
+                        <span>Phones:</span>
+                        <span>Admissions: +7 (4832) 58-82-64</span>
+                        <span>Responsible Secretary of the admission</span>
+                        <span>Vysotsky Andrey: +7 (4832) 51-19-97</span>
+                        <span>First Vice-rector for academic Affairs</span>
+                        <span>Prokofiev Alexander: +7 (4832) 56-25-09</span>
+                        <span>Preparatory courses</span>
+                        <span>Lebedeva Tatyana: +7 (4832) 58-82-42</span>
+                    </p>
+                </div>
+                <div className="col-five">
+                    <ul className="social-icons transition-smooth">
+                        <li><a href="http://vk.com/public45393943" title="vk" target="_blank" rel="noopener noreferrer">
+                            <i className="fa fa-vk" aria-hidden="true"></i></a>
+                        </li>
+                        <li><a href="https://www.facebook.com/bryanskstu" title="facebook" target="_blank" rel="noopener noreferrer">
+                            <i className="fa fa-facebook-square"></i></a>
+                        </li>
+                        <li><a href="http://www.odnoklassniki.ru/group/51457726349441" title="odnoklassniki" target="_blank" rel="noopener noreferrer">
+                            <i className="fa fa-odnoklassniki-square" aria-hidden="true"></i></a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className="col-five">
-                <ul className="social-icons transition-smooth">
-                    <li><a href="http://vk.com/public45393943" title="vk" target="_blank" rel="noopener noreferrer">
-                        <i className="fa fa-vk" aria-hidden="true"></i></a></li>
-                    <li><a href="https://www.facebook.com/bryanskstu" title="facebook" target="_blank" rel="noopener noreferrer">
-                        <i className="fa fa-facebook-square"></i></a></li>
-                    <li><a href="http://www.odnoklassniki.ru/group/51457726349441" title="odnoklassniki" target="_blank" rel="noopener noreferrer">
-                        <i className="fa fa-odnoklassniki-square" aria-hidden="true"></i></a></li>
-                </ul>
+            <div className="copyright-place">
+            <span className="copyright">© Copyright BSTU 2020</span>
             </div>
         </div>
     </footer>
