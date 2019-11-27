6bedf74fdf88d9869845220caf5ab86d0c94135e
N7_Remax <ditritusa@gmail.com>
2019-11-27 02:15:08 +0200

    installed and connect slick css theme

--- src/App/Main/University/University.js
+++ src/App/Main/University/University.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 export default class University extends Component {
   render() {
     const settings = {
