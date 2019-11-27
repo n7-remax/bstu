e6474c7f3f2e39e9587bb5eebb83d204338c1b54
N7_Remax <ditritusa@gmail.com>
2019-11-27 03:23:18 +0200

    change button in Intro module

--- src/App/Main/Intro/Intro.js
+++ src/App/Main/Intro/Intro.js
@@ -1,21 +1,23 @@
 import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
 
 import './intro.css'
 
 const Intro = () =>
-    <section className="intro">
-      <div className="row">
-        <div className="headline"><p>The place to</p></div>
-        <div className="make" aria-label="Make"><p>
-          <span>M</span><span>A</span><span>K</span><span>E</span>
-        </p></div>
-        <div className="words-container">
-          <p className="words-backgrounded">Your future</p>
-        </div>
-        <div className="button-place">
-        <a className="button" href="#course-finder">Explore our courses</a>
-        </div>
-        </div>
-    </section>
+  <section className="intro">
+    <div className="row">
+      <div className="headline"><p>The place to</p></div>
+      <div className="make" aria-label="Make"><p>
+        <span>M</span><span>A</span><span>K</span><span>E</span>
+      </p></div>
+      <div className="words-container">
+        <p className="words-backgrounded">Your future</p>
+      </div>
+      <div className="button-place">
+        <AnchorLink className="button" href='#footer'>Sign up today</AnchorLink>
+      </div>
+    </div>
+  </section>
 
 export default Intro
