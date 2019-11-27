6f174b28a6886fb015ae08a8c1d38b4f26cd2864
N7_Remax <ditritusa@gmail.com>
2019-11-27 18:21:28 +0200

    Updates

---
+++ precache-manifest.2a9b4cbe37b0504b0a4ede14fc7aa4f9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2224c4576d421ab7d28079d16f047bbf",
+    "url": "/bstu/index.html"
+  },
+  {
+    "revision": "d6655223048b67f9650b",
+    "url": "/bstu/static/css/2.321230f0.chunk.css"
+  },
+  {
+    "revision": "01ad4ce4746630aab09f",
+    "url": "/bstu/static/css/main.e7be3bdc.chunk.css"
+  },
+  {
+    "revision": "d6655223048b67f9650b",
+    "url": "/bstu/static/js/2.7c4c9177.chunk.js"
+  },
+  {
+    "revision": "01ad4ce4746630aab09f",
+    "url": "/bstu/static/js/main.4285fc98.chunk.js"
+  },
+  {
+    "revision": "3308574716c9ab1f3204",
+    "url": "/bstu/static/js/runtime-main.ad7502d5.js"
+  },
+  {
+    "revision": "67d1c21f5525887eaa816f986c165bcc",
+    "url": "/bstu/static/media/about-university-2.67d1c21f.jpg"
+  },
+  {
+    "revision": "0ea39b6a7511592c1ec12c32b5dfa076",
+    "url": "/bstu/static/media/about-university.0ea39b6a.jpg"
+  },
+  {
+    "revision": "0ff77a6f9114059159208b3fbbcf8fb6",
+    "url": "/bstu/static/media/back-inf.0ff77a6f.jpg"
+  },
+  {
+    "revision": "e6fb7a98a08ca283cadefbc10bfeeae1",
+    "url": "/bstu/static/media/bryansk-city-arms.e6fb7a98.png"
+  },
+  {
+    "revision": "3c4f22abdd8164efcc77140bc82e9945",
+    "url": "/bstu/static/media/bryansk.3c4f22ab.jpg"
+  },
+  {
+    "revision": "2db81e5c3a7ae578a590415ab0d2ea3c",
+    "url": "/bstu/static/media/engineer.2db81e5c.jpg"
+  },
+  {
+    "revision": "05ef392afbefcad63d9f0bfeecb47373",
+    "url": "/bstu/static/media/logo.05ef392a.png"
+  },
+  {
+    "revision": "d8e05ac362af4beca4b49fbb86695c39",
+    "url": "/bstu/static/media/park.d8e05ac3.jpg"
+  },
+  {
+    "revision": "ef33ea6f5bb3a23987a0f49e45bbd394",
+    "url": "/bstu/static/media/scientist-2.ef33ea6f.png"
+  },
+  {
+    "revision": "877d03150cd92740f0998d7dcf661f9b",
+    "url": "/bstu/static/media/scientist.877d0315.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/bstu/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/bstu/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/bstu/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/bstu/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9ac398f7b1af5531e667af6c330a8289",
+    "url": "/bstu/static/media/slide-1.9ac398f7.jpg"
+  },
+  {
+    "revision": "16fe1608f3e58a6e5e2ec13c80f610b5",
+    "url": "/bstu/static/media/slide-2.16fe1608.jpg"
+  },
+  {
+    "revision": "d3b5d8dfbaacf640fbf39c5bfc64392e",
+    "url": "/bstu/static/media/slide-3.d3b5d8df.jpg"
+  },
+  {
+    "revision": "bbce59066c9f0e2dc9498447553044f3",
+    "url": "/bstu/static/media/slide-4.bbce5906.jpg"
+  },
+  {
+    "revision": "0c94bfa4833acaa28c6b0180e105b6ea",
+    "url": "/bstu/static/media/students.0c94bfa4.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bstu/precache-manifest.2a9b4cbe37b0504b0a4ede14fc7aa4f9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bstu/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
