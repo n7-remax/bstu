67172d6859e268f15d30654ce6e40371fa804b43
N7_Remax <ditritusa@gmail.com>
2019-11-27 02:47:53 +0200

    created and connect AboutUniversity module

---
+++ src/App/Main/AboutUniversity/AboutUniversity.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './AboutUniversity.css'
+
+const AboutUniversity = () =>
+    <div className="about-university">
+    </div>
+
+export default AboutUniversity
--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -9,6 +9,7 @@ import Vision from './Vision/Vision';
 import Bryansk from './Bryansk/Bryansk';
 import Science from './Science/Science';
 import University from './University/University';
+import AboutUniversity from './AboutUniversity/AboutUniversity';
 
 function Main() {
   return (
@@ -19,6 +20,7 @@ function Main() {
       <Route exact path="/" render={() => <University/>}/>
       <Route path="/bryansk" component={Bryansk}/>
       <Route path="/our-vision" component={Science}/>
+      <Route path="/about-university" component={AboutUniversity}/>
 
     </main>
   );
