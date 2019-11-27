d3d09b41448d671b7dd5161bc770c138654e23fb
N7_Remax <ditritusa@gmail.com>
2019-11-27 03:43:59 +0200

    added new logo prototype and renew nav

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -2,19 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
+import logo from '../../common/images/logo/logo.jpg'
+
 
 import './header.css'
 
 const Header = () =>
     <header className="header">
         <div className="logo-place">
-            <Link to="/"><img className="logo" src={require('./logo.png')} alt="logo"></img></Link>
+            <Link to="/"><img className="logo" src={logo} alt="logo"></img></Link>
             <h1><Link to="/">Bryansk State Technical University</Link></h1>
         </div>
         <nav className="nav">
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/our-vision">About us</Link></li>
+                <li><Link to="/about-university">Our University</Link></li>
                 <li><Link to="/bryansk">Explore our city</Link></li>
                 <li><AnchorLink href='#footer'>Contact us</AnchorLink></li>
             </ul>
