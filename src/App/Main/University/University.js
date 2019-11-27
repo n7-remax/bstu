69d414752c7d323fbf29df32d0a8e4ed005b6c1b
N7_Remax <ditritusa@gmail.com>
2019-11-27 02:45:03 +0200

    added button to University module

--- src/App/Main/University/University.js
+++ src/App/Main/University/University.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+import { Link } from 'react-router-dom'
+
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -24,20 +26,23 @@ export default class University extends Component {
                     <blockquote>“A better University - For a better world”</blockquote>
                 </div>
                 <div className="slider-place slider-row">
-                <Slider {...settings}>
-                    <div>
-                    <img className="slider-image" src={require('../../../common/images/slider-images/slide-1.jpg')} alt="university"></img>
+                    <Slider {...settings}>
+                        <div>
+                            <img className="slider-image" src={require('../../../common/images/slider-images/slide-1.jpg')} alt="university"></img>
+                        </div>
+                        <div>
+                            <img className="slider-image" src={require('../../../common/images/slider-images/slide-2.jpg')} alt="logo"></img>
+                        </div>
+                        <div>
+                            <img className="slider-image" src={require('../../../common/images/slider-images/slide-3.jpg')} alt="logo"></img>
+                        </div>
+                        <div>
+                            <img className="slider-image" src={require('../../../common/images/slider-images/slide-4.jpg')} alt="logo"></img>
+                        </div>
+                    </Slider>
+                    <div className="button-place">
+                        <Link className="button" to="/about-university">Learn more about our University</Link>
                     </div>
-                    <div>
-                    <img className="slider-image" src={require('../../../common/images/slider-images/slide-2.jpg')} alt="logo"></img>
-                    </div>
-                    <div>
-                    <img className="slider-image" src={require('../../../common/images/slider-images/slide-3.jpg')} alt="logo"></img>
-                    </div>
-                    <div>
-                    <img className="slider-image" src={require('../../../common/images/slider-images/slide-4.jpg')} alt="logo"></img>
-                    </div>
-                </Slider>
                 </div>
             </div>
         );
