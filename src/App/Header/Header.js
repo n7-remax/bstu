bd5a219ec0361467a94625a7c8a484878016f8b8
N7_Remax <ditritusa@gmail.com>
2020-03-10 20:24:20 +0200

    rework navbar

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -14,13 +14,51 @@ const Header = () =>
             <h1><Link to="/bstu">Bryansk State Technical University</Link></h1>
         </div>
         <nav className="nav">
-            <ul>
+            <div className="box">
+                {/* <ul>
                 <li><Link to="/bstu">Home</Link></li>
                 <li><Link to="/our-vision">About us</Link></li>
                 <li><Link to="/about-university">Our University</Link></li>
                 <li><Link to="/bryansk">Explore our city</Link></li>
                 <li><AnchorLink href='#footer'>Contact us</AnchorLink></li>
-            </ul>
+            </ul> */}
+                <Link to="/bstu">Home</Link>
+                <div class="dropdown">
+                    <button class="dropbtn">About us</button>
+                    <div class="dropdown-content">
+                    <Link to="/our-vision">About BSTU</Link>
+                        <a href="#">History</a>
+                        <a href="#">International department</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <button class="dropbtn">Our speciality</button>
+                    <div class="dropdown-content">
+                        <a href="#">Full education</a>
+                        <a href="#">Past education</a>
+                        <a href="#">Paid</a>
+                        <a href="#">Free</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <button class="dropbtn">International studies resource</button>
+                    <div class="dropdown-content">
+                        <a href="#">Map of Bryansk</a>
+                        <a href="#">Our Campuses</a>
+                        <a href="#">Transfer</a>
+                        <a href="#">The first stap</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <button class="dropbtn">Migration department</button>
+                    <div class="dropdown-content">
+                        <a href="#">Invitation</a>
+                        <a href="#">Visa</a>
+                        <a href="#">Registration</a>
+                        <a href="#">Memo</a>
+                    </div>
+                </div>
+            </div>
         </nav>
     </header>
 
